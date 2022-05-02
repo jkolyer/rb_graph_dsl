bd9f9c28a1852a846a98a4fd0ece9ab7597d04c1
Jonathan Kolyer <jkolyer@users.noreply.github.com>
2022-05-02 16:33:44 -0400

    Graph settings (#1)
    * added graph.to_s
    
    * renamed expected_value to expected_result
    
    * refactoring GraphSettings
    
    Co-authored-by: Jonathan Kolyer <jonathankolyer@gmail>

--- graphs.rb
+++ graphs.rb
@@ -1,69 +1,69 @@
+require 'logger'
 class AlgorithmResultError < StandardError; end
 
 class Graph
-  @config_attrs = [:id, :node_count, :edges, :directed, :algorithm, :expected_value].freeze
-  @no_expected_value = Object.new
-  
+  @config_attrs = [:id, :node_count, :edges, :directed, :algorithm, :expected_result].freeze
+  @no_expected_result = Object.new
+
   def self.config_attrs
     @config_attrs
   end
   attr_accessor(*self.config_attrs)
 
+  @@logger = Logger.new(STDOUT)
+
   def initialize
     # this value is not required
-    self.expected_value = @no_expected_value
+    self.expected_result = @no_expected_result
     # we default to directed graph
     self.directed = true
+  end
+
+  def to_s
+    "#{self.class.name}(#{id}): #{node_count} nodes"
   end
 
   def run_algorithm
     raise ArgumentError.new("Found nil argument #{self.class.config_attrs}") \
       if self.class.config_attrs.map{|at| send(at)}.any?(nil)
 
+    log_perf { "start #{id}" } 
     result = send(algorithm)
+    log_perf { "finish #{id}:  result = #{result}" } 
     
-    if _has_expected_value? && result != expected_value
-      raise AlgorithmResultError.new("#{algorithm} expecting #{expected_value}; instead got #{result}")
+    if _has_expected_result? && result != expected_result
+      raise AlgorithmResultError.new("#{algorithm} expecting #{expected_result}; instead got #{result}")
     end
     result
   end
 
+  ############
+  # Algorithms
+  ############
+  
   def has_cycle?
     directed ? _directed_has_cycle? : _undirected_has_cycle?
   end
 
   def topological_sort
-    @visited = _init_node_count_false_array
-    @topo_stack = []
-
-    def _dfs_topo_sort(vtx)
-      @visited[vtx] = true
-      return @topo_stack << vtx if _adj_list[vtx].empty?
-
-      _adj_list[vtx].each do |adj_vtx|
-        next if @visited[adj_vtx]
-        _dfs_topo_sort(adj_vtx)
-      end
-      @topo_stack << vtx
-    end
-    
-    (0...node_count).each do |vtx|
-      next if @visited[vtx]
-      _dfs_topo_sort(vtx) 
-    end
-    @topo_stack.reverse    
+    raise ArgumentError.new() unless directed
+    _topological_sort
   end
 
   #########
   protected
   #########
 
+  def log_perf(&block)
+    @@logger.info(&block) if GraphConfig::GraphSettings.track_performance
+  end
+
   def _init_node_count_false_array
     Array.new(node_count, false)
   end
 
-  def _has_expected_value?
-    self.expected_value != @no_expected_value
+  def _has_expected_result?
+    self.expected_result != @no_expected_result
   end
 
   def _adj_list
@@ -132,6 +132,28 @@ class Graph
     
     _dfs_cycle(0)
     @has_cycle
+  end
+
+  def _topological_sort
+    @visited = _init_node_count_false_array
+    @topo_stack = []
+
+    def _dfs_topo_sort(vtx)
+      @visited[vtx] = true
+      return @topo_stack << vtx if _adj_list[vtx].empty?
+
+      _adj_list[vtx].each do |adj_vtx|
+        next if @visited[adj_vtx]
+        _dfs_topo_sort(adj_vtx)
+      end
+      @topo_stack << vtx
+    end
+    
+    (0...node_count).each do |vtx|
+      next if @visited[vtx]
+      _dfs_topo_sort(vtx) 
+    end
+    @topo_stack.reverse    
   end
 
 end
